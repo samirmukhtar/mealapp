4b7296007dd34b4c162c4912f11fe1a5f45ac7c7
samir mukhtar <samirsamawade@gmail.com>
2026-09-08 09:43:57 +0300

    viewModel scope

--- app/src/main/java/com/horncoder/mealapp/ui/meal/MealViewModel.kt
+++ app/src/main/java/com/horncoder/mealapp/ui/meal/MealViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.horncoder.mealapp.model.MealRepository
 import com.horncoder.mealapp.model.response.Category
 import kotlinx.coroutines.CoroutineScope
@@ -12,23 +13,16 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class MealViewModel(private val repository: MealRepository = MealRepository()): ViewModel() {
-    private val mealJob = Job()
 
     init {
-        val scope = CoroutineScope(mealJob + Dispatchers.IO)
 
-        scope.launch() {
+        viewModelScope.launch(Dispatchers.IO) {
             val meal = getMeal()
             mealState.value = meal
         }
     }
 
     val mealState: MutableState<List<Category>> =  mutableStateOf(emptyList())
-
-    override fun onCleared() {
-        super.onCleared()
-        mealJob.cancel()
-    }
 
     private suspend fun getMeal(): List<Category>{
         return repository.getMeal().categories
